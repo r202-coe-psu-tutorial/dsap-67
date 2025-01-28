078cce9b913188db6237baebdd193674551ac4a1
Thanathip Limna <boatkrap@gmail.com>
2025-01-28 09:59:03 +0700

    feat: add username and password verification

--- kivy-app/simple/hello.py
+++ kivy-app/simple/hello.py
@@ -1,8 +1,8 @@
 from kivy.app import App
-from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
+from kivy.uix.label import Label
 
 
 class HelloApp(App):
@@ -18,6 +18,9 @@ class HelloApp(App):
         self.password = TextInput(hint_text="Password", password=True)
 
         submit_button = Button(text="Submit")
+        submit_button.bind(on_press=self.check_credentials)
+
+        self.result_label = Label(text="Result will be shown here.")
 
         username_layout.add_widget(label_username)
         username_layout.add_widget(self.username)
@@ -28,8 +31,15 @@ class HelloApp(App):
         layout.add_widget(username_layout)
         layout.add_widget(password_layout)
         layout.add_widget(submit_button)
+        layout.add_widget(self.result_label)
 
         return layout
+
+    def check_credentials(self, instance):
+        if self.username.text == "admin" and self.password.text == "admin":
+            self.result_label.text = "Login successful!"
+        else:
+            self.result_label.text = "Invalid username or password."
 
 
 if __name__ == "__main__":
